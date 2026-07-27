65ab0e5de57e5dc92b373b29ab328d86a862df9e
Pallavi1603 <pallaviyelagandula789@gmail.com>
2026-07-27 17:00:26 +0530

    Initial commit

--- clientGemini_multi.py
+++ clientGemini_multi.py
@@ -9,4 +9,49 @@ from typing import Any
 from google import genai
 from mcp import ClientSession,StdioServerParameters
 from mcp.client.stdio import stdio_client
+   
 
+CONFIG_FILE= os.path.join(os.path.dirname(__file__),"config.json")
+MODEL="gemini-3.5-flash"
+
+
+class MultiServerMCPClient:
+    
+    def __init__(self):
+        self.exit_stack=AsyncExitStack()
+        self.sessions:dict[str,ClientSession]={}
+        self.client=genai.Client()
+        self.tool_to_server:dict[str,str]={}
+        
+        
+    async def connect_to_server(
+        self,
+        name:str,
+        command:str,
+        args:list[str],
+        env:dict[str,str]|None):
+        merged_env={**os.environ} 
+        if env:
+            merged_env.update(
+                {
+                k:v for k,v in env.items() if v and "PUT_YOUR" not in v
+                })
+        
+        params=StdioServerParameters(
+            command=command,
+            args=args,
+            env=merged_env
+        )
+        
+        stdio_transport=await self.exit_stack.enter_async_context(stdio_client(params))
+        read_stream,write_stream=stdio_transport 
+        session=await self.exit_stack.enter_async_context(ClientSession(read_stream,write_stream))
+        
+        await session.initialize()
+        self.sessions[name]=session
+        print(f" connected :{name} ")
+        
+        
+    
+    
+    
--- gemini_loop.py
+++ gemini_loop.py
@@ -9,10 +9,10 @@ from mcp.client.stdio import stdio_client
 
 print("GEMINI_API_KEY =", os.getenv("GEMINI_API_KEY"))
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+# client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-for model in client.models.list():
-    print(model.name)
+# for model in client.models.list():
+#     print(model.name)
 
 MODEL = "gemini-3.5-flash"
 
